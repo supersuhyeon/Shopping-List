8e62da8fc316c143ba250113ddc68a631b78f2bc
supersuhyeon <suhyeonkr@gmail.com>
2022-05-22 23:39:37 +0900

    revised JS

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const input = document.querySelector('.text-input')
 const btn = document.querySelector('.material-icons')
-
 const todoList = document.querySelector('.list')
 
 const typingText = () => { 
-//   console.log(input.value);
-//   const addelem = document.createElement('li')
-//   addelem.innerText = input.value
-//   todoList.appendChild(addelem)
-//   input.value = '';
 
+// 0. bring the input's value
+const text = input.value
+if(text === ''){
+    input.focus();
+    return; //stop
+}
+//   console.log(input.value);
+
+// 1. create the new elements & append child
 const listSection = document.createElement('div')
 const viewText = document.createElement('div')
 const viewIcon = document.createElement('button')
 const span = document.createElement('span')
 const i = document.createElement('span')
-const text = input.value
 
 listSection.className = 'list-section'
 viewText.className = 'viewText'
@@ -34,11 +36,19 @@ viewText.appendChild(span)
 viewIcon.appendChild(i)
 
 viewIcon.addEventListener('click', (event)=>{
+    // console.log(event.target)
+    // console.log(event.target.parentNode)
+    // console.log(event.target.parentNode.parentNode)
     const removeIcon = event.target.parentNode.parentNode
     todoList.removeChild(removeIcon)
 })
-input.value = ''
 
+// 2. scroll to a new elememt created
+listSection.scrollIntoView({block:'end'})
+
+// 3. reset all input's value
+input.value = ''
+input.focus();
 }
 
 
